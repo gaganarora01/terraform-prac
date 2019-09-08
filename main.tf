d5bd020dd7d8327d9e7b732f7d0b116a700f9eee
gaganarora01 <gagan.arora.83@gmail.com>
2019-09-08 16:05:03 +0530

    Added index count

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "firstdemo" {
+  count = 3
   ami = "ami-922914f7"
   instance_type = "t2.micro"
 
   tags {
-    Name = "demoinstance"
+    Name = "demoinstance-${count.index}"
   }
 }
