11b1119c2e8360be6f85a0647e7392f51ff34206
gaganarora01 <gagan.arora.83@gmail.com>
2019-09-16 23:25:00 +0530

    Added gcp files

---
+++ gcp/modules/uc1/variables.tf
@@ -0,0 +1,27 @@
+variable "company" {
+        default = "company-name"
+    }    
+    
+variable "env" {
+        default = "dev"
+    }
+    
+variable "var_region_name" { 
+        default = "company-name"
+    }
+    
+variable "uc1_private_subnet" {
+        default = "10.26.1.0/24"
+    }
+    
+variable "uc1_public_subnet" {
+        default = "10.26.2.0/24"
+    }
+    
+variable "ue1_private_subnet" {
+        default = "10.26.3.0/24"
+    }
+    
+variable "ue1_public_subnet" {
+        default = "10.26.4.0/24"
+    }
